afdbb6d50aef04d75dbd55af7c94a127576aa9c2
victor.moreno <victor.moreno.borras@estudiantat.upc.edu>
2024-03-31 15:32:23 +0200

    feat: modification .sh

--- scripts/all_run_vad.sh
+++ scripts/all_run_vad.sh
@@ -1,7 +1,36 @@
 #! /bin/bash
 
-for alpha1 in $(seq 4 0.1 6); do
-    alpha1_dot=$(echo "$alpha1" | tr ',' '.')
-    echo -n "$alpha1_dot: "
-    scripts/run_vad.sh $alpha1_dot | grep TOTAL
-done
+best_total=0
+best_alpha1=0
+best_alpha2=0
+best_min_silence=0
+best_min_voice=0
+best_init_counter=0
+
+init_counter=16
+min_silence=8
+min_voice=3
+
+for alpha1 in $(seq 6 1 10); do
+    for alpha2 in $(seq 0 1 4); do
+        # for min_silence in $(seq 0 2 10); do
+        #     for min_voice in $(seq 0 2 10); do
+                    alpha1_dot=$(echo "$alpha1" | tr ',' '.')
+                    alpha2_dot=$(echo "$alpha2" | tr ',' '.')
+                    echo -n "alpha1=$alpha1, alpha2=$alpha2, min_silence=$min_silence, min_voice=$min_voice, init_counter=$init_counter   RESULT:"
+                    total=$(scripts/run_vad.sh $alpha1_dot $alpha2_dot $min_silence $min_voice $init_counter | grep TOTAL | awk '{print $3}' | tr -d '%')
+                    echo "$total%"
+
+                    if (( $(echo "$total > $best_total" | bc -l) )); then
+                        best_total=$total
+                        best_alpha1=$alpha1
+                        best_alpha2=$alpha2
+                        best_min_silence=$min_silence
+                        best_min_voice=$min_voice
+                        best_init_counter=$init_counter
+                    fi
+done done 
+# done done
+
+echo "Best total value: $best_total"
+echo "Best parameters: alpha1=$best_alpha1, alpha2=$best_alpha2, min_silence=$best_min_silence, min_voice=$best_min_voice, init_counter=$best_init_counter"
--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -5,6 +5,10 @@
 
 # Assigning alpha1 argument to a variable
 alpha1=${1:-5}
+alpha2=${2:-5}
+min_silence=${3:-0.1}
+min_voice=${4:-0}
+init_counter=${5:-0}
 
 # Establecemos que el código de retorno de un pipeline sea el del último programa con código de retorno
 # distinto de cero, o cero si todos devuelven cero.
@@ -15,22 +19,19 @@ DIR_P2=$HOME/PAV/P2
 DB=$DIR_P2/db.v4
 CMD=$DIR_P2/bin/vad
 
+zcr_stv=3400 zcr_vts=3600
+
 for filewav in $DB/*/*wav; do
-#    echo
-    echo "**************** $filewav ****************"
-    if [[ ! -f $filewav ]]; then 
-	    echo "Wav file not found: $filewav" >&2
-	    exit 1
-    fi
+        echo "**************** $filewav ****************"
+        if [[ ! -f $filewav ]]; then 
+            echo "Wav file not found: $filewav" >&2
+            exit 1
+        fi
 
-    filevad=${filewav/.wav/.vad}
-    $CMD -i $filewav -o $filevad -1 $alpha1 || exit 1  
+        filevad=${filewav/.wav/.vad}
+        $CMD -i $filewav -o $filevad -1 $alpha1 -2 $alpha2 -Z $zcr_stv -X $zcr_vts -V $min_voice -S $min_silence -I $init_counter || exit 1
 
-# Alternatively, uncomment to create output wave files
-#    filewavOut=${filewav/.wav/.vad.wav}
-#    $CMD $filewav $filevad $filewavOut || exit 1
-
-done
+done 
 
 scripts/vad_evaluation.pl $DB/*/*lab
 
