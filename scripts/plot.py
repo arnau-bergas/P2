864eac555774f11acff80da8706c0516b6dfc978
victor.moreno <victor.moreno.borras@estudiantat.upc.edu>
2024-03-31 17:37:16 +0200

    feat: created python script for plotting wav files

---
+++ scripts/plot.py
@@ -0,0 +1,15 @@
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+input_data = read("/home/victhor/PAV/P2/prueba.wav")
+audio = input_data[1]
+input_data_s = read("/home/victhor/PAV/P2/prueba_silenced.wav")
+audio_s = input_data_s[1]
+
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.plot(audio[0:1024000])
+ax2.plot(audio_s[0:1024000])
+plt.show()
